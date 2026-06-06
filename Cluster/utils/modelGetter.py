e639b56af9046acec9d47c598d8dbf4f9919722e
DanielZastrau <daniel@author>
2026-06-06 13:35:46 +0200

    better model specs

--- Cluster/utils/modelGetter.py
+++ Cluster/utils/modelGetter.py
@@ -7,9 +7,10 @@ def model_getter(args: argparse.Namespace):
     data = DataProvider(args=args)
 
     return UNetModel(
+            # * Models the parameters of DistillKac
             image_size=data.data_dims.size, in_channels=data.data_dims.channels,
             out_channels=data.data_dims.channels,
-            model_channels=128, num_res_blocks=2,
-            attention_resolutions=[16], num_heads=4,
-            num_head_channels=64, channel_mult=(1, 2, 2, 2)
+            model_channels=256, num_res_blocks=3,
+            attention_resolutions=[16, 8, 4], num_heads=4,
+            num_head_channels=64, channel_mult=(1, 2, 2, 3)
         )
