99b3241ce420cfa8601228e1ac055ba439717e90
DanielZastrau <daniel@author>
2026-06-25 08:07:59 +0200

    added learning rate arg

--- Cluster/utils/argumentStandards.py
+++ Cluster/utils/argumentStandards.py
@@ -6,6 +6,6 @@ def set_standards(args: argparse.Namespace):
         # * as was done in "2025 - Duong et al - Telegraphers"
         args.time_truncation = 0
 
-    args.lr = args.lr * (args.training_batch_size / 128)
+    args.training_optimizer_lr = args.training_optimizer_lr * (args.training_batch_size / 128)
 
     return args
